bf968f4c32a4db3b5f954af14c443771f67282b6
Gach1koi <k1ss.sakura@outlook.com>
2024-02-28 21:22:17 +0800

    Initial Commit

---
+++ EmojiArt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by 落殇 on 2024/2/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by 落殇 on 2024/2/28.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
