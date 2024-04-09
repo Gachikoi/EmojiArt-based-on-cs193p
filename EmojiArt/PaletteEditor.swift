67aa716b3b605004268bfbf7c5779fec66caf409
Gach1koi <k1ss.sakura@outlook.com>
2024-04-09 22:20:20 +0800

    Add Function: In PaletteChooser, the chooser implement Edit, Go To, New Fuction.

---
+++ EmojiArt/PaletteEditor.swift
@@ -0,0 +1,18 @@
+//
+//  PaletteEditor.swift
+//  EmojiArt
+//
+//  Created by 落殇 on 2024/4/9.
+//
+
+import SwiftUI
+
+struct PaletteEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaletteEditor()
+}
